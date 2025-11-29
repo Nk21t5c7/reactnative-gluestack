ad3301e02dcd66a952896b29262ec4c620d343cc
nk21t5c7 <nodokahanaki@gmail.com>
2025-11-29 14:42:27 -0800

    Add gluestack icons to tab

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,3 +1,5 @@
+import { AddIcon, Icon, StarIcon } from '@/components/ui/icon';
+import { Colors } from '@/constants/theme';
 import { Tabs } from 'expo-router';
 import React from 'react';
 
@@ -12,17 +14,23 @@ export default function TabLayout() {
       screenOptions={{
         tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: false,
+        tabBarActiveTintColor: Colors.light.tint,
+        tabBarInactiveTintColor: Colors.light.tabIconDefault,
       }}>
       <Tabs.Screen
         name="index"
         options={{
           title: 'Home',
+          tabBarIcon: ({ color }) =>   <Icon as={StarIcon} className={`w-4 h-4`} style={{ color: color }} />
+          ,
         }}
       />
       <Tabs.Screen
         name="add"
         options={{
           title: 'Add Memo',
+          tabBarIcon: ({ color }) =>   <Icon as={AddIcon} className={`w-4 h-4`} style={{ color: color }}  />
+          ,
         }}
       />
     </Tabs>
